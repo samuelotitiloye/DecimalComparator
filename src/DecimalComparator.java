6d4a968bd19987b19f5df2ebda5a16beebf4f806
Samuel Titiloye <samuelotitiloye@gmail.com>
2020-06-11 05:16:28 -0500

    comparing decimals

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -12,8 +12,6 @@ public class DecimalComparator {
 // * areEqualByThreeDecimalPlaces(3.0, 3.0); → should return true since numbers are equal up to 3 decimal places.
 // * areEqualByThreeDecimalPlaces(-3.123, 3.123); → should return false since numbers are not equal up to 3 decimal places.
 
-// TIP: Use paper and pencil.
 // TIP: Use casting.
 
-// NOTE: The areEqualByThreeDecimalPlaces method  needs to be defined as public static like we have been doing so far in the course.
-// NOTE: Do not add a  main method to solution code.
+// NOTE: The areEqualByThreeDecimalPlaces method  needs to be defined as public static
